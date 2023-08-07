d7708309ebd4ddf7f1b5feb4ff525436756e58ff
[name] <--unset>
2023-08-07 16:17:16 -0400

    initial commit

---
+++ tests/test_window_utils.py
@@ -0,0 +1,116 @@
+import os
+import signal
+import subprocess
+import time
+
+import keyboard
+import pytest
+import win32gui
+
+from window_utils import *
+
+pids = []
+
+
+def setup_module():
+    global pids
+    pids = [
+        subprocess.Popen(["notepad"]).pid,
+        subprocess.Popen(["mspaint"]).pid,
+    ]
+    time.sleep(2)
+
+
+def teardown_module():
+    global pids
+    for pid in pids:
+        os.kill(pid, signal.SIGTERM)
+
+
+@pytest.fixture(autouse=True)
+def notepad():
+    hwnd = win32gui.FindWindow("Notepad", "Untitled - Notepad")
+    return Window(hwnd)
+
+
+@pytest.fixture(autouse=True)
+def paint():
+    hwnd = win32gui.FindWindow("MSPaintApp", "Untitled - Paint")
+    return Window(hwnd)
+
+
+def test_move(notepad: Window, paint: Window):
+    notepad.move(0, 0, 400, 400)
+    paint.move(200, 200, 400, 400)
+
+
+def test_flash(notepad: Window, paint: Window):
+    notepad.flash()
+    time.sleep(2)
+
+
+def test_activate(notepad: Window, paint: Window):
+    notepad.activate()
+    assert notepad.hwnd == win32gui.GetForegroundWindow()
+    paint.activate()
+    assert paint.hwnd == win32gui.GetForegroundWindow()
+
+
+def test_is_active(notepad: Window, paint: Window):
+    notepad.activate()
+    assert notepad.is_active()
+    assert not paint.is_active()
+    paint.activate()
+    assert paint.is_active()
+    assert not notepad.is_active()
+
+
+def test_get_rect(notepad: Window, paint: Window):
+    x, y, w, h = (-7, 3, 163, 142)
+    rect = (x, y, x + w, y + h)
+    notepad.move(x, y, w, h)
+    assert notepad.get_rect() == rect
+
+
+def test_get_bbox(notepad: Window, paint: Window):
+    x, y, w, h = (-7, 3, 163, 142)
+    bbox = (x, y, x + w, y + h)
+    notepad.move(x, y, w, h)
+    assert notepad.get_bbox() == bbox
+
+
+def test_get_size(notepad: Window, paint: Window):
+    x, y, w, h = (-7, 3, 163, 142)
+    size = (w, h)
+    notepad.move(x, y, w, h)
+    assert notepad.get_size() == size
+
+
+def test_activate_momentarily(notepad: Window, paint: Window):
+    paint.activate()
+    assert paint.is_active()
+    with notepad.activate_momentarily():
+        keyboard.write("Hello World!")
+        assert notepad.is_active()
+    assert paint.is_active()
+
+
+def test_get_foreground_window(notepad: Window, paint: Window):
+    notepad.activate()
+    assert get_foreground_window().hwnd == notepad.hwnd
+    paint.activate()
+    assert get_foreground_window().hwnd == paint.hwnd
+
+
+def test_find_window(notepad: Window, paint: Window):
+    pass
+
+
+def test_find_window_noclass(notepad: Window, paint: Window):
+    expected = notepad.hwnd
+    actual = find_window(None, "Untitled - Notepad").hwnd
+    assert actual == expected
+
+
+def test_find_window_notitle(notepad: Window, paint: Window):
+    pass
---
+++ window_utils/__init__.py
@@ -0,0 +1,81 @@
+from contextlib import contextmanager
+
+import win32com.client
+import win32con
+import win32gui
+
+
+class Window:
+    def __init__(self, hwnd) -> None:
+        self._hwnd = hwnd
+
+    @property
+    def hwnd(self) -> int:
+        return self._hwnd
+
+    def is_active(self) -> bool:
+        return self._hwnd == win32gui.GetForegroundWindow()
+
+    def get_rect(self) -> tuple[int, int, int, int]:
+        return win32gui.GetWindowRect(self._hwnd)
+
+    def get_bbox(self) -> tuple[int, int, int, int]:
+        rect = win32gui.GetWindowRect(self._hwnd)
+        left, top, right, bottom = rect
+        return (left, top, right - left, bottom - top)
+
+    def get_size(self) -> tuple[int, int]:
+        rect = win32gui.GetWindowRect(self._hwnd)
+        left, top, right, bottom = rect
+        return (right - left, bottom - top)
+
+    def flash(self) -> None:
+        flags = win32con.FLASHW_ALL | win32con.FLASHW_TIMERNOFG
+        count = 0
+        rate = 0
+        win32gui.FlashWindowEx(self._hwnd, flags, count, rate)
+
+    @contextmanager
+    def activate_momentarily(self, *args, **kwargs) -> None:
+        prev_window = get_foreground_window()
+
+        if prev_window.hwnd == self.hwnd:
+            yield
+        else:
+            try:
+                # Activate the window.
+                self.activate(*args, **kwargs)
+                yield
+            finally:
+                # Return control to the original window.
+                prev_window.activate(*args, **kwargs)
+
+    def activate(self, force=True, ignore_errors=True) -> bool:
+        try:
+            if force:
+                shell = win32com.client.Dispatch("WScript.Shell")
+                shell.SendKeys("%")
+            win32gui.SetForegroundWindow(self._hwnd)
+        except win32gui.error:
+            if ignore_errors:
+                return False
+            else:
+                raise
+        else:
+            return True
+
+    def move(self, x, y, width, height, repaint=True) -> bool:
+        # TODO if width, height are none leave the window the same size
+        try:
+            win32gui.MoveWindow(self._hwnd, x, y, width, height, repaint)
+        except Exception as e:
+            raise
+
+
+def get_foreground_window() -> Window:
+    return Window(win32gui.GetForegroundWindow())
+
+
+def find_window(_class_name, _window_title) -> Window:
+    hwnd = win32gui.FindWindow(_class_name, _window_title)
+    return Window(hwnd)
